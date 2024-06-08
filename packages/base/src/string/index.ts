59c331f78ade4d1a8dfcf8c64407bd5f94a204ed
lingann <wanglingan@mysteel.com>
2024-06-08 20:46:49 +0800

    ✨ feat: 追加新的string相关方法

--- packages/base/src/string/index.ts
+++ packages/base/src/string/index.ts
@@ -1,38 +1,5 @@
 export * from './trim'
 export * from './replace'
-
-/**
- * 获取数字
- * @param val
- * @returns
- */
-export const getStringNumber = (val?: string): number => {
-  return val ? Number(val.replace(/[^0-9]/gi, '')) : -1
-}
-
-/**
- * 获取单位
- * @param val
- * @returns
- */
-export const getStringUnit = (val: string) => {
-  return val.replace(/[0-9]*/g, '').replace(/\./, '')
-}
-
-/**
- * 拿到最后一个字符串
- * @param val
- * @returns
- */
-export const getStringLastChar = (val: string) => {
-  return val.substring(val.length - 1)
-}
-
-/**
- * 用中文字符替换逗号
- * @param str
- * @returns
- */
-export function stringReplaceCommaWithChinese(str: string) {
-  return str.replace(/,/g, '，')
-}
+export * from './case'
+export * from './uuid'
+export * from './extract'
