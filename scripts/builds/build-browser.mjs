38517a4dffa0d068b77f604bbd923bb2b868c4ec
wanglingan <wanglingan@mysteel.com>
2024-06-29 20:02:53 +0800

    ✨ feat: 重构包名

--- scripts/builds/build-browser.mjs
+++
@@ -1,6 +0,0 @@
-import { createBuildQueue } from '../rollup/build-queue.mjs'
-import { compilePackages } from '../rollup/rollup-compile.mjs'
-
-const queue = createBuildQueue()
-
-compilePackages(queue, 'dist')
